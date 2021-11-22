825cc4773cc6fdb9f96f2daff8d2a1a0f2163233
Kevin Leung <kevinresol@gmail.com>
2021-11-22 17:43:32 +0800

    Fix field not submitted by form

--- src/theme/dasloop/login/resources/js/script.js
+++ src/theme/dasloop/login/resources/js/script.js
@@ -3,8 +3,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
 	params.forEach((value, key) => {
 		const matches = key.match(/^prefill-(.*)$/);
 		if(matches) {
-			const input = document.getElementById(matches[1]);
-			input.disabled = true;
+			const input = document.querySelector('#kc-register-form input#' + matches[1]);
+			// input.disabled = true; // can't just disable the field because form will not submit a disabled field
+			input.parentElement.className = 'ui disabled input';
 			input.value = value;
 		}
 	});
